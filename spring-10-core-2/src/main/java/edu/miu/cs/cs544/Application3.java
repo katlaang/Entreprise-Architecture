476c8130b6b48dafdaf6a38336eefbddecb84ff9
katlaang <36038168+katlaang@users.noreply.github.com>
2021-04-23 21:39:14 -0500

    Add files via upload

---
+++ spring-10-core-2/src/main/java/edu/miu/cs/cs544/Application3.java
@@ -0,0 +1,21 @@
+package edu.miu.cs.cs544;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Application3 {
+	private static ApplicationContext context;
+
+	public static void main(String[] args) {
+		System.out.println("1");
+		context = new ClassPathXmlApplicationContext("springconfig3.xml");
+
+		System.out.println("2");
+		CustomerService customerService = context.getBean("customerService", CustomerService.class);
+		
+		System.out.println("3");
+		customerService.addCustomer("Frank Brown");
+		
+		System.out.println("4");
+	}
+}
