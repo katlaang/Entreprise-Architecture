b14031b19083803884225b49ef1ce4fd7b07d4e2
katlaang <36038168+katlaang@users.noreply.github.com>
2021-04-23 21:40:37 -0500

    Add files via upload

---
+++ spring-11-di-4/src/main/java/edu/miu/cs/cs544/Application.java
@@ -0,0 +1,17 @@
+package edu.miu.cs.cs544;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Application {
+	private static ApplicationContext context;
+
+	public static void main(String[] args) {
+		context = new ClassPathXmlApplicationContext("springconfig.xml");
+
+		AccountService accountService = context.getBean("accountService", AccountService.class);
+		
+		accountService.deposit(1234L, 2000);
+		
+	}
+}
